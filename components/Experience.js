7be86529e7411d2044ce998b81aadbbd8a3e00f8
Ngozi Ekekwe <ngozi-ekekwe>
2021-01-03 21:12:45 +0100

    add work experience

--- components/Experience.js
+++ components/Experience.js
@@ -1,17 +1,114 @@
+import React, { useState } from "react";
 import styled from "styled-components";
+import { experienceDirectory } from "../utils/experiences";
 
 function Experiences() {
+  const workExperience = experienceDirectory;
+  const lengthOfExperiences = workExperience.length;
+
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
   return (
     <ExperiencesStyled>
-
+      <ExperienceWrapper>
+        <SectionTitle>Experience</SectionTitle>
+        <ExperienceLayout>
+          {experienceDirectory &&
+            experienceDirectory.map((experience, index) => {
+              return (
+                <SingleExperience
+                  className={carouselIndex === index ? "active" : ""}
+                >
+                  <div>
+                    <h1>{experience.role}</h1>
+                    <h2>{experience.company}</h2>
+                    <small>{experience.timeLine}</small>
+                    <p>{experience.description}</p>
+                  </div>
+                </SingleExperience>
+              );
+            })}
+        </ExperienceLayout>
+        <NavCircleWrapper>
+          {workExperience &&
+            workExperience.map((_, index) => {
+              return (
+                <NavCircle
+                  className={carouselIndex === index ? "active" : ""}
+                  onClick={() => setCarouselIndex(index)}
+                ></NavCircle>
+              );
+            })}
+        </NavCircleWrapper>
+      </ExperienceWrapper>
     </ExperiencesStyled>
-  )
+  );
 }
 
 const ExperiencesStyled = styled.section`
-  height: 90vh;
   width: 100%;
   background-image: linear-gradient(to right, #e3ffe7, #d9e7ff);
-`
+`;
+
+const ExperienceWrapper = styled.section`
+  width: 80%;
+  overflow: hidden;
+  margin: 0 auto;
+`;
+
+const SectionTitle = styled.h3`
+  font-size: 2rem;
+  padding: 2rem 0;
+`;
+
+const ExperienceLayout = styled.div`
+  display: flex;
+  overflow: hidden;
+  padding: 2rem 0;
+  height: 100%;
+  width: 100%;
+`;
+
+const SingleExperience = styled.div`
+  width: 0%;
+  height: 300px;
+  background-color: #fafafa;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  padding: 1rem 1.5rem;
+  opacity: 0;
+  transform: translateX(-100%);
+  transition: all 0.75s ease;
+
+  &.active {
+    width: 85%;
+    margin: 0 auto;
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const NavCircleWrapper = styled.ul`
+  display: flex;
+  padding: 2rem 0;
+  align-item: center;
+  justify-content: center;
+`;
+
+const NavCircle = styled.li`
+  height: 20px;
+  width: 20px;
+  cursor: pointer;
+  list-style: none;
+  margin-right: 2rem;
+  border: 1px solid #fa6400;
+  border-radius: 50%;
+
+  &.active,
+  &:hover {
+    border: 3px solid #fa6400;
+  }
+`;
 
 export default Experiences;
---
+++ utils/experiences.js
@@ -0,0 +1,55 @@
+export const experienceDirectory = [
+  {
+    company: "Finleap Connect",
+    timeLine: "March 2022 - March 2036",
+    role: 'Frontend Engineer',
+    url: "https://optimistic-engelbart-1d97fa.netlify.app/",
+    description:
+      "A fun take on encouraging young voters to vote in the general 2019 Nigerian elections. The web app helps first time voters find the nearest location to register for the general elections",
+    tags: ["HTML", "CSS", "Vanilla JavaScript"],
+    logo: "bg3.webp",
+    class: 'active'
+  },
+
+  {
+    company: "Linux Foundation",
+    timeLine: "March 2022 - March 2036",
+    role: 'Frontend Engineer',
+    url: "https://optimistic-engelbart-1d97fa.netlify.app/",
+    description:
+      "A fun take on encouraging young voters to vote in the general 2019 Nigerian elections. The web app helps first time voters find the nearest location to register for the general elections",
+    tags: ["HTML", "CSS", "Vanilla JavaScript"],
+    logo: "bg3.webp",
+  },
+
+  {
+    company: "Dirty Lemon",
+    role: 'Frontend Engineer',
+    timeLine: "March 2022 - March 2036",
+    url: "https://optimistic-engelbart-1d97fa.netlify.app/",
+    description:
+      "A fun take on encouraging young voters to vote in the general 2019 Nigerian elections. The web app helps first time voters find the nearest location to register for the general elections",
+    tags: ["HTML", "CSS", "Vanilla JavaScript"],
+    logo: "bg3.webp",
+  },
+  {
+    company: "Tastemakers Africa",
+    role: 'Frontend Engineer',
+    timeLine: "March 2022 - March 2036",
+    url: "https://optimistic-engelbart-1d97fa.netlify.app/",
+    description:
+      "A fun take on encouraging young voters to vote in the general 2019 Nigerian elections. The web app helps first time voters find the nearest location to register for the general elections",
+    tags: ["HTML", "CSS", "Vanilla JavaScript"],
+    logo: "bg3.webp",
+  },
+  {
+    company: "Andela",
+    role: 'Software Developer',
+    timeLine: "March 2022 - March 2036",
+    url: "https://optimistic-engelbart-1d97fa.netlify.app/",
+    description:
+      "A fun take on encouraging young voters to vote in the general 2019 Nigerian elections. The web app helps first time voters find the nearest location to register for the general elections",
+    tags: ["HTML", "CSS", "Vanilla JavaScript"],
+    logo: "bg3.webp",
+  },
+];
