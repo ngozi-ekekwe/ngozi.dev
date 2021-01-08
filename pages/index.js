b995cc72bfb4ef2cd8d8cc7d213b4b6b9e919fa7
Ngozi Ekekwe <ngozi-ekekwe>
2021-01-08 20:32:56 +0100

    meta tags

--- pages/index.js
+++ pages/index.js
@@ -29,14 +29,8 @@ export default function Home({ setCurrentTheme, currentTheme }) {
         <meta charSet="UTF-8" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="Personal Portfolio site for Ngozi Ekekwe. - Frontend Engineer living and working in Berlin"
-        />
-        <meta
-          name="keywords"
-          content="Nextjs, React, Developer, Berlin, Germany, Female, Frontend developer"
-        />
+        <meta name="description" content="Personal Portfolio site for Ngozi Ekekwe. - Frontend Engineer living and working in Berlin" />
+        <meta name="keywords" content="Nextjs, React, Developer, Berlin, Germany, Female, Frontend developer" />
         <meta name="author" content="Ngozi Ekekwe" />
 
         {/* Twitter Tags */}
@@ -44,41 +38,22 @@ export default function Home({ setCurrentTheme, currentTheme }) {
         <meta name="twitter:site" content="@NgoziEkekwe" />
         <meta name="twitter:creator" content="@NgoziEkekwe"  key="twhandle"/>
         <meta name="twitter:url" content="https://ngozi.dev/" />
-        <meta
-          name="twitter:title"
-          content="Ngozi Ekekwe - Frontend Engineer."
-        />
-        <meta
-          name="twitter:image"
-          content="https://ngozi.dev/ngozi-ekekwe.jpg"
-        />
-        <meta
-          name="twitter:description"
-          content="Ngozi Ekekwe - Frontend Engineer living in Berlin."
-        />
+        <meta name="twitter:title" content="Ngozi Ekekwe - Frontend Engineer." />
+        <meta name="twitter:image" content="https://ngozi.dev/ngozi-ekekwe.jpg" />
+        <meta name="twitter:description" content="Ngozi Ekekwe - Frontend Engineer living in Berlin." />
 
         <meta property="og:url" content="https://ngozi.dev" key="ogurl" />
-        <meta
-          property="og:image"
-          content="https://ngozi.dev/ngozi-ekekwe.jpg"
-          key="ogimage"
+        <meta property="og:image" content="https://ngozi.dev/ngozi-ekekwe.jpg" key="ogimage"
         />
         <meta property="og:type" content="website" key={"ogtype"} />
         <meta property="og:title" content="Ngozi Ekekwe - Frontend Engineer." key="ogtitle" />
-        <meta
-          property="og:description"
-          content="Ngozi Ekekwe - Frontend Engineer living in Berlin."
-          key="ogdesc"
-        />
+        <meta property="og:description" content="Ngozi Ekekwe - Frontend Engineer living in Berlin." key="ogdesc" />
        
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content="Ngozi Ekekwe" key="ogsitename" />
         
         <meta property="og:image:type" content="image/jpg" />
-        <meta
-          property="og:image:alt"
-          content="Profile picture of me, Ngozi Ekekwe"
-        />
+        <meta property="og:image:alt" content="Profile picture of me, Ngozi Ekekwe" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <title>{SEO_TITLE}</title>
       </Head>
